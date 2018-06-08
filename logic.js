dabbfb2ad6537d6a010010c70e93d376cc39286f
matheusrcruz <git.dev@outlook.com>
2018-06-08 15:14:35 -0300

    New: JavaScript

---
+++ logic.js
@@ -0,0 +1,99 @@
+/*let text  = 'Hello world in js ?';
+let nome = 'Matheus'; string
+document.write(nome.length); quantididade de elementos
+document.write(nome.charAt(0)); acessar valor especifico de uma string
+document.write(text.substring(0,4)); acessar range ex de matheus 0,4, === math
+document.write(nome.toUpperCase(nome)); somente maisculo
+doment.write(nome.toLowerCase(nome)); somente minuscula
+-------------------------------------------------------------
+document.write((10 < 11) && (10 > 9) +'<br>'); operador logico < menor && E and 
+document.write((10 > 11) || (10 === 10) +'<br>'); operador logico maior > || ou
+document.write((10 < 11)+ '<br>'); 
+document.write(1 > 2? 'é maior' : 'é menor'); ternario ?
+-------------------------------------------------------------
+array e methodo
+let nome = ['hello', 1, true];
+nome.splice(0, 1);
+document.write(nome);
+-------------------------------------------------------------
+looping
+let i = 0;
+
+while (i <= 10){
+    document.write(i + '<br>');
+    i++;
+}
+loooping interarivo
+var nome =['hello', 'darkness', 'lol'];
+
+for (let i = 0; i < nome.length; i++){
+    document.write(nome[i]+'<br>');
+}
+looping padrao 
+for (let i = 0; i <= 10; i++){
+    document.write(i + '<br>')
+}
+-------------------------------------------------------------
+condicionais if else(else if);
+
+let idade = 22;
+
+if (idade < 18){
+    document.write("É menor de idade");
+}
+else if(idade ===22){
+    document.write("Voce tem 22 anos");
+    
+}
+else{
+    document.write("É maior de"+ idade);
+}
+
+for(let i = 0; i < 10; i ++) {
+    if(i === 3){
+     continue;
+    }
+    document.write(i + '<br>')
+}
+
+
+
+
+switch case ==== escolha caso?
+let idade = 19;
+
+switch (idade){
+    case 17:
+        document.write('tem 17!'+ '<br>');
+        break;
+    case 18:
+        document.write('tem 18!'+'<br>');
+        break;
+    default:
+        document.write('Não sei !'+ '<br>');
+        break;
+}
+ 1) Forma de adionar propriedade na criação do objeto 
+ ex:
+ let caneta = {
+    cor: 'preto',
+    marca: 'BIC'
+};
+
+document.write(caneta.cor)
+
+2) Forma após a criação do obj
+ex 
+let caneta = {};
+caneta.cor  = 'azul';
+ 
+objeto
+
+let caneta = {};
+caneta.cor = 'azul';
+caneta.marca= 'BIC'
+
+document.write(caneta.marca)
+*/
+
+
